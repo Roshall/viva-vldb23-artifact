6f8740f6104681e2cf19ade19b070eace3433e7f
Roshall <15504658671@163.com>
2023-05-30 10:55:40 +0800

    only inevitable models will be loaded

--- viva/nodes/node_mappings.py
+++ viva/nodes/node_mappings.py
@@ -5,7 +5,7 @@ from viva.nodes.inference_nodes import (
     EmotionDetectNode, ClassificationNode, QuantizedClassificationNode,
     ObjectTrackNode, ActionDetectNode, ProxyClassificationNode, Img2VecNode,
     TASTIObjectDetectNode, TASTIEmotionDetectNode, TASTIFaceDetectNode,
-    TASTIActionDetectNode, EmotionDetectNodeCascades,DeepFaceNode,
+    TASTIActionDetectNode, EmotionDetectNodeCascades, DeepFaceNode,
     DeepFacePrefixNode, DeepFaceSuffixNode
 )
 from viva.nodes.miscellaneous_nodes import (
@@ -13,6 +13,7 @@ from viva.nodes.miscellaneous_nodes import (
     SimpleTransferNode, OverheadNode, ComplexTransferNode, DataGeneratorNode
 )
 from viva.utils.config import ConfigManager
+
 config = ConfigManager()
 deepface_common_prefix_num_layers = config.get_value('execution', 'deepface_common_prefix_num_layers')
 
@@ -40,38 +41,56 @@ facedet_hierarchy = {
 }
 
 inp_col_list = ['framebytes', 'width', 'height']
-NodeMappings = {
-    'ts'  : TranscriptSearchNode([]),
-    'si'  : SimilarityNode(inp_col_list),
-    'bi'  : BrightnessNode(inp_col_list),
-    'svm' : SVMNode(inp_col_list),
-    'md'  : MotionNode(inp_col_list),
-    'fd'  : FaceDetectNode(inp_col_list),
-    'ed'  : EmotionDetectNode(inp_col_list),
-    'edc' : EmotionDetectNodeCascades(inp_col_list),
-    'cl'  : ClassificationNode(inp_col_list),
-    'qcl' : QuantizedClassificationNode(inp_col_list),
-    'pc'  : ProxyClassificationNode(inp_col_list),
-    'ad'  : ActionDetectNode(inp_col_list),
-    'odn' : ObjectDetectNodeNano(inp_col_list),
-    'od'  : ObjectDetectNode(inp_col_list),
-    'odm' : ObjectDetectNodeMedium(inp_col_list),
-    'odl' : ObjectDetectNodeLarge(inp_col_list),
-    'odx' : ObjectDetectNodeXLarge(inp_col_list),
-    'ot'  : ObjectTrackNode(inp_col_list),
-    'i2v' : Img2VecNode(inp_col_list),
-    'tod' : TASTIObjectDetectNode(['img2vec']),
-    'ted' : TASTIEmotionDetectNode(['img2vec']),
-    'tfd' : TASTIFaceDetectNode(['img2vec']),
-    'tad' : TASTIActionDetectNode(['img2vec']),
-    'dfage': DeepFaceNode(inp_col_list, [], 'Age'),
-    'dfgender': DeepFaceNode(inp_col_list, [], 'Gender'),
-    'dfrace': DeepFaceNode(inp_col_list, [], 'Race'),
-    'dfprefix': DeepFacePrefixNode(inp_col_list, [], deepface_common_prefix_num_layers),
-    'dfsuffixage': DeepFaceSuffixNode(['dfprefixembed'], [], 'Age', deepface_common_prefix_num_layers+1),
-    'dfsuffixgender': DeepFaceSuffixNode(['dfprefixembed'], [], 'Gender', deepface_common_prefix_num_layers+1),
-    'dfsuffixrace': DeepFaceSuffixNode(['dfprefixembed'], [], 'Race', deepface_common_prefix_num_layers+1),
-    'transfer' : ComplexTransferNode(inp_col_list),
-    'overhead' : OverheadNode(inp_col_list),
-    'simple' : SimpleTransferNode(inp_col_list),
-}
+
+
+class NodeMap:
+    def __init__(self):
+        self.NodeMappings = {
+            'ts': TranscriptSearchNode([]),
+            'si': SimilarityNode(inp_col_list),
+            'bi': BrightnessNode(inp_col_list),
+            'svm': SVMNode(inp_col_list),
+            'md': MotionNode(inp_col_list),
+            'fd': (FaceDetectNode, (inp_col_list,)),
+            'ed': (EmotionDetectNode, (inp_col_list,)),
+            'edc': (EmotionDetectNodeCascades, (inp_col_list,)),
+            'cl': (ClassificationNode, (inp_col_list,)),
+            'qcl': (QuantizedClassificationNode, (inp_col_list,)),
+            'pc': (ProxyClassificationNode, (inp_col_list,)),
+            'ad': (ActionDetectNode, (inp_col_list,)),
+            'odn': (ObjectDetectNodeNano, (inp_col_list,)),
+            'od': (ObjectDetectNode, (inp_col_list,)),
+            'odm': (ObjectDetectNodeMedium, (inp_col_list,)),
+            'odl': (ObjectDetectNodeLarge, (inp_col_list,)),
+            'odx': (ObjectDetectNodeXLarge, (inp_col_list,)),
+            'ot': (ObjectTrackNode, (inp_col_list,)),
+            'i2v': (Img2VecNode, (inp_col_list,)),
+            'tod': TASTIObjectDetectNode(['img2vec']),
+            'ted': TASTIEmotionDetectNode(['img2vec']),
+            'tfd': TASTIFaceDetectNode(['img2vec']),
+            'tad': TASTIActionDetectNode(['img2vec']),
+            'dfage': (DeepFaceNode, (inp_col_list, [], 'Age')),
+            'dfgender': (DeepFaceNode, (inp_col_list, [], 'Gender')),
+            'dfrace': (DeepFaceNode, (inp_col_list, [], 'Race')),
+            'dfprefix': (DeepFacePrefixNode, (inp_col_list, [], deepface_common_prefix_num_layers)),
+            'dfsuffixage': (DeepFaceSuffixNode, (['dfprefixembed'], [], 'Age', deepface_common_prefix_num_layers + 1)),
+            'dfsuffixgender': (
+                DeepFaceSuffixNode, (['dfprefixembed'], [], 'Gender', deepface_common_prefix_num_layers + 1)),
+            'dfsuffixrace': (
+                DeepFaceSuffixNode, (['dfprefixembed'], [], 'Race', deepface_common_prefix_num_layers + 1)),
+            'transfer': (ComplexTransferNode, (inp_col_list,)),
+            'overhead': OverheadNode(inp_col_list),
+            'simple': (SimpleTransferNode, (inp_col_list,)),
+        }
+
+    def __getitem__(self, item):
+        node_info = self.NodeMappings[item]
+        if type(node_info) is tuple:
+            self.NodeMappings[item] = node_info[0](*node_info[1])
+        return self.NodeMappings[item]
+
+    def __contains__(self, item):
+        return item in self.NodeMappings
+
+
+NodeMappings = NodeMap()
