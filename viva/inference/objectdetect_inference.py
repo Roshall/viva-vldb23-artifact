dffb8bacf209537278d7d69a9a6c3da73e3b69d3
Roshall <15504658671@163.com>
2023-06-20 19:24:15 +0800

    mute yolo

--- viva/inference/objectdetect_inference.py
+++ viva/inference/objectdetect_inference.py
@@ -19,7 +19,7 @@ class ObjectDetectInference(AbstractInference):
 
     def _prepare_model_udf(self, size = 'yolov5s'):
         yolov5 = torch.hub.load('/root/.cache/torch/hub/ultralytics_yolov5_master', 'custom', size+'.pt',
-                                source='local', verbose=False)
+                                _verbose=False, source='local', verbose=False)
         bc_yolov5_state = self.sc.broadcast(yolov5.state_dict())
         use_cuda = torch.cuda.is_available() and config.get_value('execution', 'gpu')
         device = torch.device('cuda' if use_cuda else 'cpu')
@@ -27,7 +27,7 @@ class ObjectDetectInference(AbstractInference):
         def yolo_fn():
             """Gets the broadcasted model."""
             model = torch.hub.load('/root/.cache/torch/hub/ultralytics_yolov5_master', 'custom', size+'.pt',
-                                   source='local', verbose=False)
+                                   _verbose=False, source='local', verbose=False)
             model.load_state_dict(bc_yolov5_state.value)
             model.eval()
             model.to(device)
