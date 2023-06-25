f2eac61208d4759dd956a870488b41c11de4613e
Roshall <15504658671@163.com>
2023-06-26 00:45:04 +0800

    clean up code

--- analysis/experiments/tasti_index.sh
+++ analysis/experiments/tasti_index.sh
@@ -5,24 +5,20 @@ set -x
 
 . viva_vars
 . helper.sh
-#queries=("angrybernie" "dunk" "amsterdamdock")
-queries=${queries['a']}
 proxy_thresh=0.8
 
 pushd ${viva_root}
 . env_vars
-for q in ${queries[@]}; do
+for i in b; do
+  q="${queries[$i]}"
   new_conf ${proxy_thresh} ${script_dir} ${q} 0
   output_prefix=${viva_root}/dataset/${q}/data
   cp viva/plans/${q}_tasti_plan.py viva/plans/tasti_plan.py
-#  find data/-maxdepth 1 -iname *.mp4 -delete
-#  cp ${video_in}/*.mp4 ${video_out}/
   $python viva/core/tasti.py -q ${q} -p ${output_prefix}
-#  mv ${video_out}/${q}_tasti_index.bin ${videos_prefix}/${q}/
 done
 
 cp conf.yml.orig conf.yml
 # Remove temporary Spark directories (excluding /tmp/spark-events)
-clean_spark
+#clean_spark
 
 popd
