bebc1b8f0ce9260bb11beef0e9334171344c7435
Roshall <15504658671@163.com>
2023-06-29 09:57:29 +0800

    fix outdated files

--- analysis/experiments/all_acc_sel.sh
+++ analysis/experiments/all_acc_sel.sh
@@ -6,10 +6,10 @@ proxy_thr=0.8
 hints_plan="all_hints.py"
 
 . viva_vars
-for i in a b f d; do
+for i in f; do
   q="${queries["$i"]}"
   bash run_acc_sel.sh -q "${q}" -s "${selectivity_thr}" -c "${viva_root}/dataset/${q}/canary/${canaries[$i]}" \
-  -x "${hints_plan}" -p "${proxy_thr}"
+  -x "${hints_plan}" -p "${proxy_thr}" -n ${1}
 done
 
 echo "Done."
